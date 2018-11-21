517156c44c608a72f4d1d8dde7189b61411be5ab
root <root@localhost.localdomain>
2018-11-20 21:47:45 -0500

    Added interactive buttons to website

---
+++ main.js
@@ -0,0 +1,12 @@
+function myBulbasaur() {
+    windows.location.href='root/Work/javascript-challenge/bulbasaur.html'
+}
+
+function myCharmander() {
+    windows.location.href='/charmander.html'
+}
+
+function mySquirtle() {
+    windows.location.href='/squirtle.html'
+}
+
